5032b04553df2883948ea3893919324ab4e3e42a
Nicolas Abete <nicoabt321@gmail.com>
2025-06-19 18:56:14 -0300

    terminando la tarea para realizar la primera entrega

--- ecommerce-react/src/App.jsx
+++ ecommerce-react/src/App.jsx
@@ -3,14 +3,18 @@ import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
 import NavBar from './components/NavBar';
+import ItemListContainer from './components/ItemListContainer';
 
 function App() {
   const [count, setCount] = useState(0)
+  const saludo = "Hola Mundo"
 
   return (
     
       <div className="app-container">
           <NavBar />
+          <ItemListContainer saludo={ "Bienvenido a mi Ecommerce con React" } />
+          <ItemListContainer saludo={ "Aprendiendo a reutilizar componentes usando props" } />
           <div className="card">
             <button onClick={() => setCount((count) => count + 1)}>
               count is {count}
