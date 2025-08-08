da6f89edd0436be8e0822bfbabc8bd7ddd7bf43f
Nicolas Abete <nicoabt321@gmail.com>
2025-08-08 04:37:44 -0300

    agregue el ternario que el profe pidio para la entrega, que muestre un boton para ir al carrito si se agrega un producto

--- ecommerce-react/src/components/ItemDetail/ItemDetail.jsx
+++ ecommerce-react/src/components/ItemDetail/ItemDetail.jsx
@@ -1,33 +1,39 @@
 import React from 'react';
 import "./ItemDetail.css";
 import ItemCount from '../ItemCount/ItemCount';
-import { useContext } from 'react';
+import { useContext, useState } from 'react';
 import { CartContext } from '../../context/CartContext';
-
+import { Link } from 'react-router-dom';
 
 const ItemDetail = ({ product = {} }) => {
-
     const { addProductInCart } = useContext(CartContext);
-
+    const [productAdded, setProductAdded] = useState(false);
 
     const addProduct = (quantity) => {
         const productCart = { ...product, quantity };
         //añadir el producto al carrito
         addProductInCart(productCart);
+        // Mostrar el botón "Ir al carrito"
+        setProductAdded(true);
     }
 
-
     return (
-    <div className="item-detail-container">
-        <img className="item-detail-image" src={product.image} alt="" />
+        <div className="item-detail-container">
+            <img className="item-detail-image" src={product.image} alt="" />
 
-        <div className="item-detail-info">
-            <p className="item-detail-title">{product.name}</p>
-            <p className="item-detail-description">{product.description}</p>
-            <p className="item-detail-price">${product.price}</p>
-            <ItemCount stock={product.stock} addProduct={addProduct} />
+            <div className="item-detail-info">
+                <p className="item-detail-title">{product.name}</p>
+                <p className="item-detail-description">{product.description}</p>
+                <p className="item-detail-price">${product.price}</p>
+                <ItemCount stock={product.stock} addProduct={addProduct} />
+                
+                {productAdded && (
+                    <Link to="/cart" className="go-to-cart-btn">
+                        Ir al carrito
+                    </Link>
+                )}
+            </div>
         </div>
-    </div>
     )
 }
 
